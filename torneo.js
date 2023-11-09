2e8f80614aae7992be51e5ec5c35b2548bf86eca
abivan <ivan.agudelo@innovapps.co>
2023-11-08 21:57:29 -0500

    comentario

--- torneo.js
+++ torneo.js
@@ -1,10 +1,11 @@
 /* global use, db */
 // MongoDB Tournament
 // base de datos MongoDb que permita la gestión de los participantes a un torneo deportivo, 
-//deben incluir como minimo las siguientes colecciones (deportistas, entrenadores, árbitros, 
-//encuentros deportivos, resultados y posiciones). 
-//Ustedes escogen el tipo de evento deportivo que deseen trabajar, 
-//el cual debe ser real y tener disponible los resultados y detalles del torneo deportivo.
+// deben incluir como minimo las siguientes colecciones (deportistas, entrenadores, árbitros, 
+// encuentros deportivos, resultados y posiciones). 
+// Ustedes escogen el tipo de evento deportivo que deseen trabajar, 
+// el cual debe ser real y tener disponible los resultados y detalles del torneo deportivo.
+
 use('torneo');
 
 // Firt collection.
