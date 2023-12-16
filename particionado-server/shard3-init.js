c36a43505c71544fc33df85eda6311fd84b9fe1d
abivan <ivan.agudelo@innovapps.co>
2023-12-15 20:00:08 -0500

    Nuevo nodo

---
+++ particionado-server/shard3-init.js
@@ -0,0 +1,11 @@
+rs.initiate(
+   {
+      _id: "iberoshard3",
+      version: 1,
+      members: [
+         { _id: 0, host : "mongo-shard3a:27017" },
+         { _id: 1, host : "mongo-shard3b:27017" },
+      ]
+   }
+ )
+ 
